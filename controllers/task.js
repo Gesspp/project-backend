bc0971c1021cbda0aacec3064a6b5956ceaf2c88
Gesspp <nikitatum199@gmail.com>
2023-09-05 19:45:49 +0300

    получение и добавление задач

--- controllers/task.js
+++ controllers/task.js
@@ -1,10 +1,19 @@
 const Task = require("../models/task");
 
 
-const add = async (reg, res) => {
-    res.end("Добавление задач");
+const add = async (req, res) => {
+    if (!req.body) return res.status(400).json({error: "no data"})
+    const { title, color } = req.body;
+    const task = new Task({title, color, done: false});
+    await task.save();
+    return res.status(201).json({message: "задача успешно создана"});
 }
 
+const all = async (req, res) => {
+    const tasks = await Task.find();
+    return res.status(200).json({tasks})
+}
 module.exports = {
-    add
+    add,
+    all
 }
--- routers/task.js
+++ routers/task.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { add } = require("../controllers/task");
+const { add, all } = require("../controllers/task");
 
 const router = Router();
 
 router.post("/", add);
+router.get("/", all);
 
 module.exports = router;
