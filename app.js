c9f895058ed5ba8d84e7899180c0a94b64afdcd3
Gesspp <nikitatum199@gmail.com>
2023-09-05 18:50:42 +0300

    основа

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose")
+
+
+const app = express()
+
+async function main() {
+ 
+    try{
+        await mongoose.connect("mongodb://127.0.0.1:27017/tasks");
+        app.listen(3000);
+        console.log("Сервер ожидает подключения...");
+    }
+    catch(err) {
+        return console.log(err);
+    }
+}
+
+main();
+
+process.on("SIGINT", async() => {
+      
+    await mongoose.disconnect();
+    console.log("Приложение завершило работу");
+    process.exit();
+
+});
